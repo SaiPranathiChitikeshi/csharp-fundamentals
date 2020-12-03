6b445f1969c2f51f7f4f1116f2c96419e3af2348
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-03 13:50:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-
-namespace GradeBook
+namespace c_
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-           var marks=new[]{10,20,35.6,40.4,50};
-           var l=new List<double>() {10,20,30,40,50};
-           double r=0;
-           foreach(double n in l){
-               r+=n;
-           }
-           double a=r/l.Count;
-           Console.WriteLine(a);
+            List<Employee> emplist=new List<Employee>();
+            emplist.Add(new Employee() {id=1,name="prana",salary=10000,experience=6});
+            emplist.Add(new Employee() {id=2,name="mary",salary=5000,experience=4});
+            emplist.Add(new Employee() {id=3,name="varun",salary=20000,experience=6});
+            emplist.Add(new Employee() {id=4,name="ananya",salary=8000,experience=7});
+            emplist.Add(new Employee() {id=5,name="harish",salary=9000,experience=3});
+            IsPromotable isPromotable=new IsPromotable(Promote);
+                Employee.PromoteEmployee(emplist,isPromotable);
+        }
+        public static bool Promote(Employee emp){
+            if(emp.experience>=5){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    delegate bool IsPromotable(Employee empl);
+    class Employee{
+        public int id {get;set;}
+        public string name {get;set;}
+        public int salary {get;set;}
+        public int experience {get;set;}
+        public static void PromoteEmployee(List<Employee> emplist, IsPromotable IsEliPromote){
+            foreach(Employee emp in emplist){
+                if(IsEliPromote(emp)){
+                    Console.WriteLine(emp.name+" promoted");
+                }
+            }
         }
     }
 }
