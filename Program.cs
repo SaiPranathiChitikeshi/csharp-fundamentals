c5e87038755bd79ac2d3a9a3fe4830a0f9f66e51
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-03 18:02:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var book=new Book("pranathi's grade book");
+           book.AddGrade(90);
+           book.AddGrade(60);
+           book.AddGrade(80);
+           book.GetStatistics();
+           var marks=new[]{10,20,35.6,40.4,50};
+           var l=new List<double>() {10,20,30,40,50};
+           double r=0;
+           foreach(double n in l){
+               r+=n;
+           }
+           double a=r/l.Count;
+           Console.WriteLine(a);
+        }
+    }
+}
